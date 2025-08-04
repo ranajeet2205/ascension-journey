583e3edd3ea9abb69c22e46631c60857e8df5059
Ranajeet <ranajeet.barik96@gmail.com>
2025-08-04 23:51:56 +0530

    adding md files

---
+++ dsa/src/week1_binary_tree/Inorder.java
@@ -0,0 +1,10 @@
+package week1_binary_tree;
+
+import java.util.List;
+
+public class Inorder {
+
+    public List<Integer> inorder(TreeNode node) {
+        return null;
+    }
+}
---
+++ dsa/src/week1_binary_tree/TreeNode.java
@@ -0,0 +1,6 @@
+package week1_binary_tree;
+
+public record TreeNode(
+        int value, TreeNode left, TreeNode right
+) {
+}
